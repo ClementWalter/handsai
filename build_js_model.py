859ca05e473093fa344ad27db2ac2af146517751
Clément Walter <clementw@sicara.com>
2020-09-22 17:48:59 +0200

    Create tfjs bundle with one single file

--- build_js_model.py
+++ build_js_model.py
@@ -6,9 +6,14 @@ model = load_model("serving/models/siamese_nets_classifier/1")
 
 #%% Extract encoder and support layer
 encoder = model.layers[0]
-tfjs.converters.save_keras_model(encoder, "models/encoder/keras")
 tf.saved_model.save(encoder, "models/encoder/saved_model/1")
-tfjs.converters.convert_tf_saved_model("models/encoder/saved_model/1", "models/encoder/graph")
+tfjs.converters.convert_tf_saved_model(
+    "models/encoder/saved_model/1", "models/encoder/graph", weight_shard_size_bytes=1024 * 1024 * 4 * 4
+)
+
+kernel = model.layers[1].kernel
+tf.saved_model.save(kernel, "models/kernel/saved_model/1")
+tfjs.converters.convert_tf_saved_model("models/kernel/saved_model/1", "models/kernel/graph")
 
 #%% Create fake catalog
 support_set_size = 1
